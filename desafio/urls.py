8f39f3d14192383b80c4d6072f61413c9d86f484
valeriacrigod <godoyvaleria063@gmail.com>
2023-07-15 19:44:04 -0300

    'logout'

--- desafio/urls.py
+++ desafio/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path
 
 from desafio.views import indexView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', indexView, name="index"),
-    path('login/', LoginView.as_view(template_name='usuarios/login.html'), name='login')
+    path('login/', LoginView.as_view(template_name='usuarios/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout')
 ]
