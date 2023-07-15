65d9d3901bb458b3b3be183e5d9f14040a70acc6
Darian Hiebl <dahiebl@gmail.com>
2023-07-15 18:38:46 -0300

    templates andando

--- desafio/urls.py
+++ desafio/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from desafio.views import indexView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', indexView, name="index")
 ]
